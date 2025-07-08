0726b207569ca81617fc56c0bac8a7c493740385
Anudeep <anudeep.roy@liferay.com>
2025-07-08 10:13:05 +0530

    Wordle First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,115 +1,83 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useState, useEffect } from "react";
+import { KeyContext } from "./keyContext";
+import Header from "./components/header";
+import Grid from "./components/grid";
+import Keypad from "./components/keypad/keypad";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
-export default function Home() {
+export default function Wordle() {
+  const words = ['heart', 'lions', 'cloud', 'space', 'apple'];
+  const [word, setWord] = useState("");
+  const [currentWord, setCurrentWord] = useState([]);
+  const [currentRow, setCurrentRow] = useState(0);
+  const [currentColumn, setCurrentColumn] = useState(0);
+  const [grid, setGrid] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    setCurrentWord(words[randomIndex]);
+    console.log(words[randomIndex]);
+  }, [])
+
+  function checkValue(value) {
+    if (value === "enter") { 
+      validateWord();
+    } else if (value.length === 1) {
+      enterLetter(value);
+    } else {
+      backspace();
+    }
+  }
+
+  function enterLetter(letter) {
+    console.log('letter : ', letter);
+    if (currentColumn < 5 && letter!='enter') {
+      let userRow = document.querySelector(`.board .row:nth-child(${currentRow + 1})`);
+      let userCell = userRow.querySelector(`.box:nth-child(${currentColumn + 1})`);
+      userCell.textContent = letter;
+      setCurrentColumn(currentColumn + 1);
+    }
+  }
+
+  function validateWord() {
+    if (currentColumn == 5) { 
+      let currentGuess = [];
+      let userRow = document.querySelector(`.board .row:nth-child(${currentRow + 1})`);
+      for (var i = 1; i <= 5; i++) {
+        let userCell = userRow.querySelector(`.box:nth-child(${i})`);
+        currentGuess.push(userCell.textContent);
+      }
+      setWord(currentGuess.join(''));
+      console.log(currentGuess.join(''));
+      setCurrentColumn(0);
+      setCurrentRow(currentRow + 1);
+    }
+  }
+
+  function backspace() {
+    if (currentColumn > 0) {
+      const userRow = document.querySelector(`.board .row:nth-child(${currentRow + 1})`);
+      const userCell = userRow.querySelector(`.box:nth-child(${currentColumn})`);
+      userCell.textContent = '';
+      setCurrentColumn(currentColumn - 1);
+    }
+  }
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <KeyContext value={{ checkValue }}>
+      <div className="wordle">
+        <Header />
+        <Grid />
+        <Keypad />
+      </div>
+    </KeyContext>
   );
 }
